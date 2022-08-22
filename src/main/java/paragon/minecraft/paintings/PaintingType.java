88795361bd1dc15057e09aa06020c01f21c60682
Malcolm Riley <malcolmriley@users.noreply.github.com>
2022-08-21 17:52:11 -0700

    Implement Paintings

--- src/main/java/paragon/minecraft/paintings/PaintingType.java
+++ src/main/java/paragon/minecraft/paintings/PaintingType.java
@@ -5,13 +5,29 @@ import java.util.stream.Stream;
 import net.minecraft.world.entity.decoration.Motive;
 
 public enum PaintingType {
-	
+	ARCHWAY("archway", 2),
+	BOULDERS("boulders", 2),
+	CASTLE("castle", 3),
+	DESERT_CLIFF("desert_cliff", 2),
+	FRIENDS("friends", 1),
+	GRASS_HILL("grass_hill", 3, 2),
+	MEETING("meeting", 2, 1),
+	PICNIC("picnic", 3, 2),
+	PORTRAIT("portrait", 3),
+	TRADE("trade", 3, 2),
+	TRAVELER("traveler", 3),
+	VISITOR("visitor", 1, 2),
+	ZOMBIE("zombie", 1)
 	;
 	
 	/* Internal Fields */
 	protected String NAME;
 	protected int WIDTH;
 	protected int HEIGHT;
+	
+	private PaintingType(String name, int dimensions) {
+		this(name, dimensions, dimensions);
+	}
 	
 	private PaintingType(String name, int width, int height) {
 		this.NAME = name;
