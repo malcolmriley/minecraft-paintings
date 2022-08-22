909665c82c2fba7e4b7c6b9564ac4d9e89519798
Malcolm Riley <malcolmriley@users.noreply.github.com>
2022-08-21 17:03:29 -0700

    Implement Paintings.resource()

--- src/main/java/paragon/minecraft/paintings/Paintings.java
+++ src/main/java/paragon/minecraft/paintings/Paintings.java
@@ -1,10 +1,24 @@
 package paragon.minecraft.paintings;
 
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.fml.common.Mod;
 
-@Mod("paragon_paintings")
+@Mod(Paintings.MOD_ID)
 public class Paintings {
+	
+	public static final String MOD_ID = "paragon_paintings";
 
 	public Paintings() { }
+	
+	/**
+	 * Creates a {@link ResourceLocation} in the {@value Paintings#MOD_ID} domain with
+	 * the provided suffix as a path.
+	 * 
+	 * @param suffix - The {@link ResourceLocation} path to use
+	 * @return A suitable {@link ResourceLocation}
+	 */
+	public static ResourceLocation resource(String suffix) {
+		return new ResourceLocation(Paintings.MOD_ID, suffix);
+	}
 
 }
